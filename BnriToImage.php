c44bb063804b06e9aaee169e28d311c30f880ba7
tayarsutayar <achmaddaniar@gmail.com>
2023-07-13 20:02:22 +0700

    Add Qr Code

--- BnriToImage.php
+++ BnriToImage.php
@@ -3,31 +3,17 @@
 class BnriToImage {
     private $img;
     
-    function createImage($tanggal, $nomor, $tahun, $penandatangan){
+    function createImage($tanggal, $nomor, $tahun, $penandatangan, $link_document){
 
-        $fontSize = 12;
-        $imgWidth = 600;
-        $imgHeight = 350;
-        $spacing = 20;
+        $fontSize = 100;
+        $imgWidth = 3400;
+        $imgHeight = 1750;
+        $spacing = 150;
         $angle = 0;
         //text font path
         $font = dirname(__FILE__) .'/bos.ttf';
 
-        $text = '
-        Diundangkan di Jakarta\n
-        pada tanggal '.$tanggal.'\n
-        \n
-        DIREKTUR JENDERAL\n
-        PERATURAN PERUNDANG-UNDANGAN\n
-        KEMENTERIAN HUKUM DAN HAK ASASI MANUSIA\n
-        REPUBLIK INDONESIA,\n
-        \n
-        \n
-        \n
-        '.$penandatangan.'\n
-        \n
-        \n
-        BERITA NEGARA REPUBLIK INDONESIA TAHUN '.$tahun.' NOMOR '.$nomor;
+        $text = 'Diundangkan di Jakarta\npada tanggal '.$tanggal.'\n\nDIREKTUR JENDERAL\nPERATURAN PERUNDANG-UNDANGAN\nKEMENTERIAN HUKUM DAN HAK ASASI MANUSIA\nREPUBLIK INDONESIA,\n###REPLACELOGO###\n\n\n'.$penandatangan.'\n\n\nBERITA NEGARA REPUBLIK INDONESIA TAHUN '.$tahun.' NOMOR '.$nomor;
         
         //create the image
         $this->img = imagecreatetruecolor($imgWidth, $imgHeight);
@@ -39,18 +25,55 @@ class BnriToImage {
         
         //break lines
         $splitText = explode ( "\\n" , $text );
-        $lines = 0;
+        $lines = 1;
         
         foreach($splitText as $txt){
-            $textBox = imagettfbbox($fontSize,$angle,$font,$txt);
             $x = 0;
-            $y = 0+$lines*$spacing;
+            $y = $lines*$spacing;
             $lines = $lines+1;
             
-            //add the text
-            imagettftext($this->img, $fontSize, $angle, $x, $y, $black, $font, $txt);
+            if(strpos($txt, "###REPLACELOGO###") !== false){
+                $this->createQrCode($link_document, $x, $y-$spacing, $spacing*7.5);
+            }
+            else{
+                imagettftext($this->img, $fontSize, $angle, $x, $y, $black, $font, $txt);
+            }
         }
-	return true;
+	    return true;
+    }
+
+    private function createQrCode($text, $x, $y, $res_width){
+        $path_logo = dirname(__FILE__).'/logo-kumham.png';
+        $file_name= "temp_qr.png";
+        $file_path = dirname(__FILE__).'/'.$file_name;
+
+        QRcode::png($text, $file_path, "H", 20, 4, 0, "#fff", "#fff");
+
+        $QR = imagecreatefrompng($file_path);
+
+        // memulai menggambar logo dalam file qrcode
+        $logo = imagecreatefromstring(file_get_contents($path_logo));
+
+        imagecolortransparent($logo , imagecolorallocatealpha($logo , 0, 0, 0, 127));
+        imagealphablending($logo , false);
+        imagesavealpha($logo , true);
+
+        $QR_width = imagesx($QR);
+        $QR_height = imagesy($QR);
+
+        $logo_width = imagesx($logo);
+        $logo_height = imagesy($logo);
+
+        // Scale logo to fit in the QR Code
+        $logo_qr_width = $QR_width/8;
+        $scale = $logo_width/$logo_qr_width;
+        $logo_qr_height = $logo_height/$scale;
+
+        imagecopyresampled($QR, $logo, $QR_width/2.3, $QR_height/2.3, 0, 0, $logo_qr_width, $logo_qr_height, $logo_width, $logo_height);
+
+        imagepng($QR,$file_path);
+
+        imagecopyresampled($this->img, $QR, $x, $y, 0, 0, $res_width, $res_width, $logo_width, $logo_height);
     }
     
     function showImage(){
@@ -58,9 +81,15 @@ class BnriToImage {
         return imagepng($this->img);
     }
     
-    function saveImage($fileName, $location = ''){
+    function saveToJpg($fileName, $location = ''){
         $fileName = $fileName.".jpg";
         $fileName = !empty($location)?$location.$fileName:$fileName;
         return imagejpeg($this->img, $fileName);
+    }
+
+    function saveToPng($fileName, $location = ''){
+        $fileName = $fileName.".png";
+        $fileName = !empty($location)?$location.$fileName:$fileName;
+        return imagepng($this->img, $fileName);
     }
 }
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 //include BnriToImage class
 require_once 'BnriToImage.php';
+require_once "phpqrcode/qrlib.php"; 
 
 //create img object
 $img = new BnriToImage;
@@ -10,8 +11,10 @@ $tanggal = '26 Mei 2023';
 $nomor = '40';
 $tahun = '2023';
 $penandatangan = 'ASEP N. MULYANA';
+$link_document = 'https://peraturan.go.id/files/bn78-2023.pdf';
 
-$img->createImage($tanggal, $nomor, $tahun, $penandatangan);
+$img->createImage($tanggal, $nomor, $tahun, $penandatangan, $link_document);
 
-$img->saveImage('BNRI-Tahun'.$tahun.'-Nomor'.$nomor);
+$img->saveToPng('BNRI-Tahun'.$tahun.'-Nomor'.$nomor);
+
 ?>
